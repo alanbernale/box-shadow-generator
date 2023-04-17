6be81e17db4d577418b8da09cb02804171e87d6d
Alan Bernal <abernalespinoza@outlook.com>
2023-04-17 01:40:54 -0500

    Funcionamiento basico de la aplicacion.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 import { useState } from 'react'
 import { IconClipboardCheck } from '@tabler/icons-react'
 import GithubCorner from 'react-github-corner'
+import hexToRgb from './hexToRgb'
 import './App.css'
 
 function App () {
   const [dark, setDark] = useState(false)
+  const [offsetX, setOffsetX] = useState(0)
+  const [offsetY, setOffsetY] = useState(10)
+  const [blurRadius, setBlurRadius] = useState(15)
+  const [spreadRadius, setSpreadRadius] = useState(-3)
+  const [color, setColor] = useState('#000000')
+  const [opacity, setOpacity] = useState(0.1)
 
-  const handleChange = () => setDark(!dark)
+  const handleDark = () => setDark(!dark)
+  const handleOffsetX = (e) => setOffsetX(e.target.value)
+  const handleOffsetY = (e) => setOffsetY(e.target.value)
+  const handleBlurRadius = (e) => setBlurRadius(e.target.value)
+  const handleSpreadRadius = (e) => setSpreadRadius(e.target.value)
+  const handleColor = (e) => setColor(e.target.value)
+  const handleOpacity = (e) => setOpacity(e.target.value)
+
+  const boxShadow = offsetX + 'px' + ' ' + offsetY + 'px' + ' ' + blurRadius + 'px' + ' ' + spreadRadius + 'px' + ' rgba(' + hexToRgb(color) + ',' + opacity + ')'
 
   return (
     <div className={dark ? 'application dark' : 'application'}>
@@ -23,8 +38,10 @@ function App () {
                   <input
                     type='number'
                     className='form-control'
+                    value={offsetX}
                     min='-100'
                     max='100'
+                    onChange={handleOffsetX}
                   />
                   <span>px.</span>
                 </div>
@@ -33,8 +50,10 @@ function App () {
                 <input
                   type='range'
                   className='form-range'
+                  value={offsetX}
                   min='-100'
                   max='100'
+                  onChange={handleOffsetX}
                 />
               </div>
             </div>
@@ -45,8 +64,10 @@ function App () {
                   <input
                     type='number'
                     className='form-control'
+                    value={offsetY}
                     min='-100'
                     max='100'
+                    onChange={handleOffsetY}
                   />
                   <span>px.</span>
                 </div>
@@ -55,8 +76,10 @@ function App () {
                 <input
                   type='range'
                   className='form-range'
+                  value={offsetY}
                   min='-100'
                   max='100'
+                  onChange={handleOffsetY}
                 />
               </div>
             </div>
@@ -67,8 +90,10 @@ function App () {
                   <input
                     type='number'
                     className='form-control'
+                    value={blurRadius}
                     min='0'
                     max='100'
+                    onChange={handleBlurRadius}
                   />
                   <span>px.</span>
                 </div>
@@ -77,8 +102,10 @@ function App () {
                 <input
                   type='range'
                   className='form-range'
+                  value={blurRadius}
                   min='0'
                   max='100'
+                  onChange={handleBlurRadius}
                 />
               </div>
             </div>
@@ -89,8 +116,10 @@ function App () {
                   <input
                     type='number'
                     className='form-control'
+                    value={spreadRadius}
                     min='-100'
                     max='100'
+                    onChange={handleSpreadRadius}
                   />
                   <span>px.</span>
                 </div>
@@ -99,8 +128,10 @@ function App () {
                 <input
                   type='range'
                   className='form-range'
+                  value={spreadRadius}
                   min='-100'
                   max='100'
+                  onChange={handleSpreadRadius}
                 />
               </div>
             </div>
@@ -112,7 +143,9 @@ function App () {
                     <input
                       type='text'
                       className='form-control'
+                      value={color}
                       size='4'
+                      onChange={handleColor}
                     />
                   </div>
                 </div>
@@ -120,6 +153,8 @@ function App () {
                   <input
                     type='color'
                     className='form-color'
+                    value={color}
+                    onChange={handleColor}
                   />
                 </div>
               </div>
@@ -131,9 +166,11 @@ function App () {
                     <input
                       type='number'
                       className='form-control'
+                      value={opacity}
                       min='0'
                       max='1'
                       step='0.01'
+                      onChange={handleOpacity}
                     />
                   </div>
                 </div>
@@ -141,9 +178,11 @@ function App () {
                   <input
                     type='range'
                     className='form-range'
+                    value={opacity}
                     min='0'
                     max='1'
                     step='0.01'
+                    onChange={handleOpacity}
                   />
                 </div>
               </div>
@@ -151,11 +190,11 @@ function App () {
           </div>
           <div className='application__result'>
             <div className='application__preview'>
-              <div className='application__box' />
+              <div className='application__box' style={{ boxShadow }} />
             </div>
             <div className='application__generated'>
               <div className='application__code'>
-                box-shadow:
+                box-shadow: {boxShadow}
               </div>
               <button type='button' className='application__copy'>
                 Copy <IconClipboardCheck />
@@ -178,7 +217,7 @@ function App () {
                 type='checkbox'
                 className='color-mode__checkbox'
                 checked={dark}
-                onChange={handleChange}
+                onChange={handleDark}
               />
               <span className='color-mode__slider' />
             </label>
